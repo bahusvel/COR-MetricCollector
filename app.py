9c0d5dd936e08fc2c944e891729dec8072b88224
Denis Lavrov <bahus.vel@gmail.com>
2016-04-06 03:02:48 +0200

    Made collectd start automatically

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import time
+from subprocess import Popen
 
 from cor.api import Message, CORModule
 from cor.comm import static_router_factory
 
 from collectd.collectd import Collectd
 from viz3d.viz3d import Viz3D
+
+p = Popen(["collectd", "-C", "/root/metric/collectd/collectd.conf", "-f"])
 
 viz = Viz3D()
 collector = Collectd(route_callback=static_router_factory({"SENSOR_READING":viz}))
